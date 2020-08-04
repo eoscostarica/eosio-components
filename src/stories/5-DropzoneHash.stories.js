251a5961aa96402b83a7ae98a41a8414787dd2fe
JustinCast <justincastillovalladares@gmail.com>
2020-08-04 13:37:26 -0600

    refactor: dropzone base functionality needs to be restructured in order to use code and provide better encapsulation and incoming implementation of new features

--- src/stories/5-DropzoneHash.stories.js
+++ src/stories/5-DropzoneHash.stories.js
@@ -17,19 +17,13 @@ export default {
   component: DropzoneHash
 }
 
-export const DropzoneModal = () => (
-  <DropzoneHash useModal handleOnDropFile={action('File')} />
-)
 export const dropzone = () => {
   const classes = useStyles()
 
   return (
-    <Box height={200}>
-      <DropzoneHash
-        useModal={false}
-        handleOnDropFile={action('File')}
-        customStyle={classes.dropBox}
-      />
-    </Box>
+    <DropzoneHash
+      handleOnDropFile={action('File')}
+      customStyle={classes.dropBox}
+    />
   )
 }
