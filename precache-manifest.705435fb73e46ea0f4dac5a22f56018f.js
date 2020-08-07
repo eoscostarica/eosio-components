a09de7f0e10af0e2427b3dbbad0e65df07ae374b
JustinCast <justincastillovalladares@gmail.com>
2020-08-07 16:45:13 +0000

    Deploying to gh-pages from  @ 69d071fe85034fb681ff1616305eac3af9002cbc 🚀

---
+++ precache-manifest.705435fb73e46ea0f4dac5a22f56018f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9122c8aa584e22f07211877e50523c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e398996aced31edeeedf.bundle.js"
+  },
+  {
+    "url": "runtime~main.e398996aced31edeeedf.bundle.js"
+  },
+  {
+    "url": "vendors~main.e398996aced31edeeedf.bundle.js"
+  },
+  {
+    "url": "vendors~main.e398996aced31edeeedf.bundle.js.LICENSE.txt"
+  }
+]);
