7b0e8020cc35f304e7361f7cc50659f787b377cd
Teto Gomez <terencio17@rocketmail.com>
2021-09-16 20:19:25 -0600

    fix(webapp): code review changes

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -18,34 +18,6 @@ import Styles from './styles'
 const SHA256_REGEX_VALIDATOR = /\b[A-Fa-f0-9]{64}\b/
 const useStyles = makeStyles(Styles)
 
-// const DropzoneBox = styled(Box)({
-//   width: "100%",
-//   height: "100%",
-//   backgroundColor: "#efefef",
-//   cursor: "pointer",
-//   display: "flex",
-//   flexDirection: "column",
-//   justifyContent: "space-evenly",
-//   border: "dashed 1px #212121",
-//   borderRadius: 2,
-//   alignContent: "center",
-//   textAlign: "center",
-//   textTransform: "uppercase",
-//   padding: "2%",
-//   "&": {
-//     textAlign: "center",
-//   },
-// });
-
-// const DropzoneButton = styled(Button)({
-//   width: "236px",
-//   height: "36px",
-//   border: ".5px solid #efefwf",
-//   borderRadius: "2px",
-//   marginLeft: "auto",
-//   marginRight: "auto",
-// });
-
 const DropzoneBase = ({
   fileChange,
   progressChange,
