4793cce2d1b42951ce16bae930329c8db119af3a
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2021-10-27 08:01:10 -0600

    fix(bpjsongenerator): make save field validation

--- src/lib/utils/validation.js
+++ src/lib/utils/validation.js
@@ -9,8 +9,7 @@ const urlInputValidation = (value) => {
 }
 
 const emailInputValidation = (email) => {
-  const emailRegex =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   return emailRegex.test(email)
 }
@@ -18,35 +17,32 @@ const emailInputValidation = (email) => {
 const formInputValidation = (formData) => {
   const result = {
     candidate_name: {
-      isError: !formData.candidate_name.length,
+      isError: !!!formData.candidate_name,
       message: 'Candidate Name is required'
     },
     email: {
-      isError: !formData.email.length || !emailInputValidation(formData.email),
-      message: formData.email.length
-        ? 'Invalid Email format'
-        : 'Email is required'
+      isError: !!!formData.email || !emailInputValidation(formData.email),
+      message: !!formData.email ? 'Invalid Email format' : 'Email is required'
     },
     website: {
-      isError:
-        !formData.website.length || !urlInputValidation(formData.website),
-      message: formData.website.length
+      isError: !!!formData.website || !urlInputValidation(formData.website),
+      message: !!formData.website
         ? 'Invalid format to URL for Website'
         : 'Website is required'
     },
     code_of_conduct: {
       isError:
-        !formData.code_of_conduct.length ||
+        !!!formData.code_of_conduct ||
         !urlInputValidation(formData.code_of_conduct),
-      message: formData.code_of_conduct.length
+      message: !!formData.code_of_conduct
         ? 'Invalid format to URL for Code of Conduct'
         : 'Code of Conduct is required'
     },
     ownership_disclosure: {
       isError:
-        !formData.code_of_conduct.length ||
+        !!!formData.code_of_conduct ||
         !urlInputValidation(formData.code_of_conduct),
-      message: formData.code_of_conduct.length
+      message: !!formData.code_of_conduct
         ? 'Invalid format to URL for Ownership Disclosure'
         : 'Ownership Disclosure is required'
     }
